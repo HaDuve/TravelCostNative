4994d8bb7bc3e0a8a952cfc24547d73f24e70232
HaDuve <hionoe@hotmail.de>
2022-07-16 10:19:07 +0800

    tut: learning about flexboxes and styles (#1)

--- App/App.js
+++ App/App.js
@@ -1,14 +1,35 @@
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [enteredCostText, setEnteredCostText] = useState("");
+  const [courseCosts, setCourseCosts] = useState([]);
+  function goalInputHandler(enteredText) {
+    setEnteredCostText(enteredText);
+  }
+  function addCostHandler() {
+    setCourseCosts((currentCourseCosts) => [
+      ...currentCourseCosts,
+      enteredCostText,
+    ]);
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your new Cost!"></TextInput>
-        <Button title="New Cost"></Button>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your new Cost!"
+          onChangeText={goalInputHandler}
+        ></TextInput>
+        <Button title="New Cost" onPress={addCostHandler}></Button>
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        {courseCosts.map((cost) => (
+          <View key={cost} style={styles.costItem}>
+            <Text style={styles.costText}>{cost}</Text>
+          </View>
+        ))}
       </View>
     </View>
   );
@@ -16,18 +37,37 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
-    borderColor: '#000000',
+    borderColor: "#cccccc",
     borderWidth: 1,
     width: "70%",
     marginRight: 10,
     marginBottom: 10,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 6,
+  },
+  costItem: {
+    margin: 8,
+    padding: 6,
+    backgroundColor: "#146bbdba",
+    borderRadius: 3,
+  },
+  costText: {
+    color: "white",
   },
 });

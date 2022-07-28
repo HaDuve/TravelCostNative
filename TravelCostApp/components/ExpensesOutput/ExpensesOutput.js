d128201ff509c91dbaf68191b37d1476db2984b6
HaDuve <hionoe@hotmail.de>
2022-07-28 11:04:10 +0800

    tut 8: added fallback Text to output

--- TravelCostApp/components/ExpensesOutput/ExpensesOutput.js
+++ TravelCostApp/components/ExpensesOutput/ExpensesOutput.js
@@ -1,14 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 import { GlobalStyles } from "../../constants/styles";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
-function ExpensesOutput({ expenses, expensesPeriod }) {
+function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
+  let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+  if (expenses.length > 0) {
+    content = <ExpensesList expenses={expenses} />;
+    
   return (
     <View style={styles.container}>
       <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-      <ExpensesList expenses={expenses} />
+      {content}
     </View>
   );
 }
@@ -22,5 +26,11 @@ const styles = StyleSheet.create({
     paddingTop: 24,
     paddingBottom: 0,
     backgroundColor: GlobalStyles.colors.primary700,
+  },
+  infoText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+    marginTop: 32,
   },
 });
--- TravelCostApp/screens/RecentExpenses.js
+++ TravelCostApp/screens/RecentExpenses.js
@@ -15,7 +15,11 @@ function RecentExpenses() {
   });
 
   return (
-    <ExpensesOutput expenses={recentExpenses} expensesPeriod="Last 7 Days" />
+    <ExpensesOutput
+      expenses={recentExpenses}
+      expensesPeriod="Last 7 Days"
+      fallbackText="No expenses registered for the last 7 days."
+    />
   );
 }
 

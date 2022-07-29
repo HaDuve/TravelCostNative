34852dd71280cd482bd7b27888f7303ecdce707c
HaDuve <hionoe@hotmail.de>
2022-07-29 09:23:22 +0800

    tut 9: adding styling

--- TravelCostApp/components/ManageExpense/Input.js
+++ TravelCostApp/components/ManageExpense/Input.js
@@ -1,14 +1,41 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 const Input = ({ label, textInputConfig }) => {
+  let inputStyles = [styles.input];
+  if (textInputConfig && textInputConfig.multiline) {
+    inputStyles.push(styles.inputMultiline);
+  }
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={inputStyles} {...textInputConfig} />
     </View>
   );
 };
 
 export default Input;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 16,
+    marginVertical: 4,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top",
+  },
+});

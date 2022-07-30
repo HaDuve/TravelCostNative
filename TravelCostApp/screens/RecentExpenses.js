590863c5e776c3bcdfd9a420b1d966484750a48f
HaDuve <hionoe@hotmail.de>
2022-07-30 10:57:21 +0800

    tut10: fetching in recent expenses

--- TravelCostApp/screens/RecentExpenses.js
+++ TravelCostApp/screens/RecentExpenses.js
@@ -1,11 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
+import { fetchExpenses } from "../util/http";
 
 function RecentExpenses() {
   const expensesCtx = useContext(ExpensesContext);
+
+  useEffect(() => {
+    async function getExpenses() {
+      const expenses = await fetchExpenses();
+      expensesCtx.setExpenses(expenses);
+    }
+
+    getExpenses();
+  }, []);
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
     const today = new Date();

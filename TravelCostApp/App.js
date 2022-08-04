b4269959a58455814eb167fd03a120c94ad622a7
Hannes Duve <hannes.duve@outlook.com>
2022-08-04 16:25:47 +0700

    added auth content (#12)

--- TravelCostApp/App.js
+++ TravelCostApp/App.js
@@ -1,21 +1,81 @@
 import { StatusBar } from "expo-status-bar";
+import { useContext, useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Ionicons } from "@expo/vector-icons";
 
+import SignupScreen from "./screens/SignupScreen";
+import LoginScreen from "./screens/LoginScreen";
+
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
 import { GlobalStyles } from "./constants/styles";
 import IconButton from "./components/UI/IconButton";
-import ExpensesContextProvider from "./store/expenses-context";
+import AuthContextProvider, { AuthContext } from "./store/auth-context";
+import ExpensesContextProvider, {
+  ExpensesContext,
+} from "./store/expenses-context";
 
 const Stack = createNativeStackNavigator();
+const AuthStack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
-function ExpensesOverview() {
+function NotAuthenticatedStack() {
+  return (
+    <AuthStack.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.primary500 },
+        headerTintColor: "white",
+        contentStyle: { backgroundColor: Colors.primary100 },
+      }}
+    >
+      <AuthStack.Screen name="Login" component={LoginScreen} />
+      <AuthStack.Screen name="Signup" component={SignupScreen} />
+    </AuthStack.Navigator>
+  );
+}
+
+function AuthenticatedStack() {
+  return (
+    <ExpensesContextProvider>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+          headerTintColor: "white",
+        }}
+      >
+        <Stack.Screen
+          name="ExpensesOverview"
+          component={ExpensesOverview}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ManageExpense"
+          component={ManageExpense}
+          options={{
+            presentation: "modal",
+          }}
+        />
+      </Stack.Navigator>
+    </ExpensesContextProvider>
+  );
+}
+
+function AuthNavigation() {
+  const authCtx = useContext(AuthContext);
+
+  return (
+    <NavigationContainer>
+      {!authCtx.isAuthenticated && <NotAuthenticatedStack />}
+      {authCtx.isAuthenticated && <AuthenticatedStack />}
+    </NavigationContainer>
+  );
+}
+
+function ExpensesOverview(authCtx) {
   return (
     <BottomTabs.Navigator
       screenOptions={({ navigation }) => ({
@@ -61,33 +121,39 @@ function ExpensesOverview() {
   );
 }
 
+function Root() {
+  const [isTryingLogin, setIsTryingLogin] = useState(true);
+
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem("token");
+
+      if (storedToken) {
+        authCtx.authenticate(storedToken);
+      }
+
+      setIsTryingLogin(false);
+    }
+
+    fetchToken();
+  }, []);
+
+  if (isTryingLogin) {
+    return <AppLoading />;
+  }
+
+  return <AuthNavigation />;
+}
+
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <ExpensesContextProvider>
-        <NavigationContainer>
-          <Stack.Navigator
-            screenOptions={{
-              headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-              headerTintColor: "white",
-            }}
-          >
-            <Stack.Screen
-              name="ExpensesOverview"
-              component={ExpensesOverview}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="ManageExpense"
-              component={ManageExpense}
-              options={{
-                presentation: "modal",
-              }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </ExpensesContextProvider>
+      <AuthContextProvider>
+        <Root />
+      </AuthContextProvider>
     </>
   );
 }
---
+++ TravelCostApp/components/Auth/Input.js
@@ -0,0 +1,53 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+
+import { GlobalStyles } from "../../constants/styles";
+
+function Input({
+  label,
+  keyboardType,
+  secure,
+  onUpdateValue,
+  value,
+  isInvalid,
+}) {
+  return (
+    <View style={styles.inputContainer}>
+      <Text style={[styles.label, isInvalid && styles.labelInvalid]}>
+        {label}
+      </Text>
+      <TextInput
+        style={[styles.input, isInvalid && styles.inputInvalid]}
+        autoCapitalize={false}
+        keyboardType={keyboardType}
+        secureTextEntry={secure}
+        onChangeText={onUpdateValue}
+        value={value}
+      />
+    </View>
+  );
+}
+
+export default Input;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginVertical: 8,
+  },
+  label: {
+    color: "white",
+    marginBottom: 4,
+  },
+  labelInvalid: {
+    color: Colors.error500,
+  },
+  input: {
+    paddingVertical: 8,
+    paddingHorizontal: 6,
+    backgroundColor: GlobalStyles.colors.primary100,
+    borderRadius: 4,
+    fontSize: 16,
+  },
+  inputInvalid: {
+    backgroundColor: Colors.error100,
+  },
+});

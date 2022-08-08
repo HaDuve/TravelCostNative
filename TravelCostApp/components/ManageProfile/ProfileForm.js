20c6ff3f626310e0c807b9fe13277482490dd859
Hannes Duve <hannes.duve@outlook.com>
2022-08-08 20:57:14 +0700

    added Inputfields for Profile Name #6

--- TravelCostApp/components/ManageProfile/ProfileForm.js
+++ TravelCostApp/components/ManageProfile/ProfileForm.js
@@ -1,5 +1,7 @@
 import { useContext } from "react";
+import { View, Text } from "react-native";
 import { AuthContext } from "../../store/auth-context";
+import Input from "../ManageExpense/Input";
 
 const ProfileForm = ({
   onCancel,
@@ -14,7 +16,15 @@ const ProfileForm = ({
       value: defaultValues ? defaultValues.name : "",
       isValid: true,
     },
+    dailybudget: {
+      value: defaultValues ? defaultValues.dailybudget : "",
+      isValid: true,
+    },
   });
+
+  useLayoutEffect(() => {
+    AuthCtx.
+  }, [inputs]);
 
   function inputChangedHandler(inputIdentifier, enteredValue) {
     setInputs((curInputs) => {
@@ -24,6 +34,70 @@ const ProfileForm = ({
       };
     });
   }
+
+  return (
+    <View style={styles.form}>
+      <Text>ProfileForm</Text>
+      <Input
+        label="Name"
+        textInputConfig={{
+          onChangeText: inputChangedHandler.bind(this, "name"),
+          value: inputs.name.value,
+        }}
+        invalid={!inputs.name.isValid}
+      />
+      <Input
+        style={styles.rowInput}
+        label="Daily Budget"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: inputChangedHandler.bind(this, "dailybudget"),
+          value: inputs.dailybudget.value,
+        }}
+        invalid={!inputs.dailybudget.isValid}
+      />
+    </View>
+  );
 };
 
 export default ProfileForm;
+
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginTop: 5,
+    marginBottom: 24,
+    textAlign: "center",
+  },
+  categoryRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+  errorText: {
+    textAlign: "center",
+    color: GlobalStyles.colors.error500,
+    margin: 8,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 8,
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});

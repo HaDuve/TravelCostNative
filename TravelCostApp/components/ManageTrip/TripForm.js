12e4212090139055e2589b3811275c5ea00029d3
Hannes Duve <hannes.duve@outlook.com>
2022-08-12 21:39:37 +0700

    created TripForm file #6

---
+++ TravelCostApp/components/ManageTrip/TripForm.js
@@ -0,0 +1,171 @@
+import { useState, useContext, useEffect } from "react";
+import { View, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
+import { AuthContext } from "../../store/auth-context";
+import { fetchUser, updateUser } from "../../util/http";
+import { Button } from "react-native";
+
+import Input from "../ManageExpense/Input";
+import ErrorOverlay from "../UI/ErrorOverlay";
+import LoadingOverlay from "../UI/LoadingOverlay";
+
+const ProfileForm = () => {
+  const Trip = useContext(TripContext);
+
+  const [inputs, setInputs] = useState({
+    tripName: {
+      value: Trip.tripName ? Trip.tripName : "",
+      isValid: true,
+    },
+    totalBudget: {
+      value: Trip.totalBudget ? Trip.totalBudget : "",
+      isValid: true,
+    },
+    // TODO: decide if this is maybe not a good state to handle??
+    travellerList: {
+      value: Trip.travellerList ? Trip.travellerList : [],
+      isValid: true,
+    },
+  });
+
+  function inputChangedHandler(inputIdentifier, enteredValue) {
+    setInputs((curInputs) => {
+      return {
+        ...curInputs,
+        [inputIdentifier]: { value: enteredValue, isValid: true },
+      };
+    });
+  }
+
+  async function submitHandler(e) {
+    // TODO: adjust this function
+    const userData = {};
+    userData.userName = inputs.userName.value;
+    userData.dailybudget = +inputs.dailybudget.value;
+    userData.homeCountry = inputs.homeCountry.value;
+    userData.homeCurrency = inputs.homeCurrency.value;
+    userData.lastCountry = inputs.lastCountry.value;
+    userData.lastCurrency = inputs.lastCurrency.value;
+
+    User.addUser(userData);
+    await updateUser(AuthCtx.uid, userData);
+  }
+
+  return (
+    // TODO: adjust this input form
+    <View style={styles.form}>
+      <Text style={styles.title}>{inputs.userName.value} Profile</Text>
+      <Input
+        label="Name"
+        textInputConfig={{
+          onChangeText: inputChangedHandler.bind(this, "userName"),
+          value: inputs.userName.value,
+        }}
+        invalid={!inputs.userName.isValid}
+      />
+      <Input
+        style={styles.rowInput}
+        label="Daily Budget"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: inputChangedHandler.bind(this, "dailybudget"),
+          value: inputs.dailybudget.value,
+        }}
+        invalid={!inputs.dailybudget.isValid}
+      />
+
+      <View style={styles.inputsRow}>
+        <Input
+          style={styles.rowInput}
+          label="homeCountry"
+          textInputConfig={{
+            onChangeText: inputChangedHandler.bind(this, "homeCountry"),
+            value: inputs.homeCountry.value,
+          }}
+          invalid={!inputs.homeCountry.isValid}
+        />
+        <Input
+          style={styles.rowInput}
+          label="homeCurrency"
+          textInputConfig={{
+            onChangeText: inputChangedHandler.bind(this, "homeCurrency"),
+            value: inputs.homeCurrency.value,
+          }}
+          invalid={!inputs.homeCurrency.isValid}
+        />
+      </View>
+      <View style={styles.inputsRow}>
+        <Input
+          style={styles.rowInput}
+          label="lastCountry"
+          textInputConfig={{
+            onChangeText: inputChangedHandler.bind(this, "lastCountry"),
+            value: inputs.lastCountry.value,
+          }}
+          invalid={!inputs.lastCountry.isValid}
+        />
+        <Input
+          style={styles.rowInput}
+          label="lastCurrency"
+          textInputConfig={{
+            onChangeText: inputChangedHandler.bind(this, "lastCurrency"),
+            value: inputs.lastCurrency.value,
+          }}
+          invalid={!inputs.lastCurrency.isValid}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button style={styles.button} title="SAVE" onPress={submitHandler}>
+          SAVE
+        </Button>
+        <Button style={styles.button} title="LOGOUT" onPress={AuthCtx.logout}>
+          LOGOUT
+        </Button>
+      </View>
+    </View>
+  );
+};
+
+export default ProfileForm;
+
+const styles = StyleSheet.create({
+  form: {
+    flex: 1,
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginTop: 5,
+    marginBottom: 24,
+    textAlign: "center",
+  },
+  categoryRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+  errorText: {
+    textAlign: "center",
+    color: GlobalStyles.colors.error500,
+    margin: 8,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 8,
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8,
+  },
+});

60e1ef31fce9b39fa6a86a092da36d73603b5c31
Hannes Duve <hannes.duve@outlook.com>
2022-09-12 15:25:10 +0800

    Added monthly comparison expense budget #38 #34

--- TravelCostApp/components/ExpensesOutput/ExpenseStatistics/ExpenseGraph.js
+++ TravelCostApp/components/ExpensesOutput/ExpenseStatistics/ExpenseGraph.js
@@ -1,27 +1,62 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import React, { useContext } from "react";
 import { ExpensesContext } from "../../../store/expenses-context";
-import { toShortFormat } from "../../../util/date";
+import { daysBetween, toMonthString, toShortFormat } from "../../../util/date";
+import { UserContext } from "../../../store/user-context";
 
 const ExpenseGraph = ({ expenses }) => {
   console.log("ExpenseGraph ~ expenses", expenses);
   const ExpenseCtx = useContext(ExpensesContext);
-  const { firstDay, lastDay, monthlyExpenses } =
-    ExpenseCtx.getMonthlyExpenses(0);
-  console.log("ExpenseGraph ~ firstDay", toShortFormat(firstDay));
-  console.log("ExpenseGraph ~ lastDay", toShortFormat(lastDay));
-  console.log("ExpenseGraph ~ monthlyExpenses", monthlyExpenses);
+  const UserCtx = useContext(UserContext);
 
-  // const expensesSum = expenses.reduce((sum, expense) => {
-  //   return sum + expense.amount;
-  // }, 0);
+  // list the last months and compare their respective expenseSum to their budget
+  const listExpenseSumBudgets = [];
+  const lastMonths = 12;
+  for (let i = 0; i < lastMonths; i++) {
+    const { firstDay, lastDay, monthlyExpenses } =
+      ExpenseCtx.getMonthlyExpenses(i);
+    const expensesSum = monthlyExpenses.reduce((sum, expense) => {
+      return sum + expense.amount;
+    }, 0);
+    const monthlyBudget = UserCtx.dailybudget * 30;
+    const obj = { firstDay, lastDay, expensesSum, monthlyBudget };
+    listExpenseSumBudgets.push(obj);
+  }
+
+  console.log("ExpenseGraph ~ listExpenseSumBudgets", listExpenseSumBudgets);
+
+  function renderItem({ item }) {
+    // const fday = toShortFormat(item.firstDay);
+    // const lday = toShortFormat(item.lastDay);
+    const month = toMonthString(item.firstDay);
+
+    console.log("renderItem ~ fday", item.firstDay);
+    return (
+      <View style={styles.itemContainer}>
+        <Text>
+          {month} {item.firstDay.getFullYear()}
+        </Text>
+        <Text>
+          {item.expensesSum}$ / {item.monthlyBudget}$
+        </Text>
+      </View>
+    );
+    // return <Text>{item.expensesSum}</Text>;
+  }
   return (
-    <View>
-      <Text>6 Months ExpenseGraph</Text>
+    <View style={styles.container}>
+      <FlatList data={listExpenseSumBudgets} renderItem={renderItem}></FlatList>
     </View>
   );
 };
 
 export default ExpenseGraph;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 4,
+  },
+  itemContainer: {
+    padding: 4,
+  },
+});
--- TravelCostApp/util/date.js
+++ TravelCostApp/util/date.js
@@ -28,6 +28,33 @@ export function toShortFormat(date) {
   return `${day} ${monthName}, ${year}`;
 }
 
+export function toMonthString(date) {
+  let monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let monthIndex = date.getMonth();
+  let monthName = monthNames[monthIndex];
+  return monthName;
+}
+
 export function getDateMinusDays(date, days) {
   return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
+}
+
+export function daysBetween(date_1, date_2) {
+  let difference = date_1.getTime() - date_2.getTime();
+  let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+  return TotalDays;
 }

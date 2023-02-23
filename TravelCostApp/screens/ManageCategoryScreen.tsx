78afa00295bc84c3982bfad277277bc2423db552
Hannes Duve <hannes.duve@outlook.com>
2023-02-23 16:28:28 +0530

    prepare ManageCategoryScreen for feature #43

--- TravelCostApp/screens/ManageCategoryScreen.tsx
+++ TravelCostApp/screens/ManageCategoryScreen.tsx
@@ -1,17 +1,30 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import PropTypes from "prop-types";
+import Button from "../components/UI/Button";
+import FlatButton from "../components/UI/FlatButton";
 
 const ManageCategoryScreen = (props) => {
-  const { storeCatlist } = props;
+  const { storeCatlist, navigation } = props;
   console.log("ManageCategoryScreen ~ storeCatlist:", storeCatlist);
   return (
-    <View>
-      <Text>ManageCategoryScreen</Text>
+    <View style={styles.container}>
+      <Text>Icon</Text>
+      <Text>Name</Text>
+      <Text>Color</Text>
+      <FlatButton onPress={navigation.popToTop}>Cancel</FlatButton>
+      <Button onPress={navigation.popToTop}>Add</Button>
     </View>
   );
 };
 
 export default ManageCategoryScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});

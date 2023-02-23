279f2ab6f4516cb3a8248876a8c328d95e7a3e5c
Hannes Duve <hannes.duve@outlook.com>
2023-02-23 10:24:00 +0530

    prettier loading overlay with text

--- TravelCostApp/components/UI/LoadingOverlay.tsx
+++ TravelCostApp/components/UI/LoadingOverlay.tsx
@@ -2,10 +2,12 @@ import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import React from "react";
 import { GlobalStyles } from "../../constants/styles";
 
+const loadingColor = GlobalStyles.colors.primaryGrayed;
 const LoadingOverlay = (containerStyle) => {
   return (
     <View style={[styles.container, containerStyle]}>
-      <ActivityIndicator size={"large"} color={"black"} />
+      <ActivityIndicator size={"large"} color={loadingColor} />
+      <Text style={styles.text}>{"Loading your Trip ... "}</Text>
     </View>
   );
 };
@@ -19,5 +21,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 24,
     backgroundColor: GlobalStyles.colors.backgroundColor,
+  },
+  text: {
+    color: loadingColor,
+    fontSize: 18,
+    fontWeight: "300",
+    marginTop: 12,
   },
 });

57123c6186b6592cbbd3c11c4fb5895bf2a447e6
Hannes Duve <hannes.duve@outlook.com>
2023-03-09 11:07:58 +0530

    cc

--- TravelCostApp/confApp.ts
+++ TravelCostApp/confApp.ts
@@ -1,12 +1,12 @@
-// Debug asyncStorage, set to true if you want all storage to be reset and user logged out
+// asyncStorage, set to true if you want all storage to be reset and user logged out
 export const DEBUG_RESET = false;
-// Debug OfflineMode, set to true if you want the simulator to be offline
+// OfflineMode, set to true if you want the simulator to be offline
 export const DEBUG_FORCE_OFFLINE = false;
-// Debug no data, set to true if you want to load no data
+// no data, set to true if you want to load no data
 export const DEBUG_NO_DATA = false;
-// Debug polling interval data
+// polling interval data
 export const DEBUG_POLLING_INTERVAL = 5000;
-// Debug polling interval online status
+// polling interval online status
 export const DEBUG_POLLING_INTERVAL_ONLINE = 10000;
 // Dev constant
 export const DEV = false;

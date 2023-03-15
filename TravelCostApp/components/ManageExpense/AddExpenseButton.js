dbb51c32a2686f478090cf6e2118e024e2edc55c
Hannes Duve <hannes.duve@outlook.com>
2023-03-15 14:42:44 +0530

    better addButton fadein

--- TravelCostApp/components/ManageExpense/AddExpenseButton.js
+++ TravelCostApp/components/ManageExpense/AddExpenseButton.js
@@ -2,7 +2,13 @@ import React, { Dimensions, Pressable, StyleSheet, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 import IconButton from "../UI/IconButton";
 import * as Haptics from "expo-haptics";
-import Animated, { FadeInDown, FadeOutDown } from "react-native-reanimated";
+import Animated, {
+  FadeIn,
+  FadeInDown,
+  FadeOutDown,
+  SlideInDown,
+  SlideInUp,
+} from "react-native-reanimated";
 import { UserContext } from "../../store/user-context";
 import { useContext } from "react";
 import { Ionicons } from "@expo/vector-icons";
@@ -11,34 +17,37 @@ const AddExpenseButton = ({ navigation }) => {
   const userCtx = useContext(UserContext);
   const isOnline = userCtx.isOnline;
   return (
-    <Pressable
-      style={({ pressed }) => [
-        styles.addButton,
-        pressed && GlobalStyles.pressedWithShadow,
-      ]}
-      onPress={() => {
-        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-        navigation.navigate("CategoryPick");
-      }}
+    <Animated.View
+      style={styles.margin}
+      entering={FadeIn.duration(600).delay(3000)}
     >
-      <Animated.View entering={FadeInDown.duration(600)}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.addButton,
+          pressed && GlobalStyles.pressedWithShadow,
+        ]}
+        onPress={() => {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+          navigation.navigate("CategoryPick");
+        }}
+      >
         <Ionicons name={"add-outline"} size={42} color={"white"} />
-      </Animated.View>
-    </Pressable>
+      </Pressable>
+    </Animated.View>
   );
 };
 
 export default AddExpenseButton;
 
 const styles = StyleSheet.create({
+  margin: { marginTop: "-100%", marginHorizontal: "40%" },
   addButton: {
     backgroundColor: GlobalStyles.colors.primary400,
     borderRadius: 999,
-    marginHorizontal: "40%",
-    marginTop: "-100%",
-    marginBottom: "1%",
-    paddingVertical: "3.8%",
-    paddingLeft: "0.8%",
+
+    marginBottom: "10%",
+    paddingVertical: "19.8%",
+    paddingLeft: "5.5%",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",

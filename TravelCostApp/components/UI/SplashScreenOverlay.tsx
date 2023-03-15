0964e68cc1cd38bd67f5c647168569d3e88c7938
Hannes Duve <hannes.duve@outlook.com>
2023-03-15 14:56:18 +0530

    add nice splashscreen animation

--- TravelCostApp/components/UI/SplashScreenOverlay.tsx
+++ TravelCostApp/components/UI/SplashScreenOverlay.tsx
@@ -9,19 +9,45 @@ import {
 } from "react-native";
 import React from "react";
 import { GlobalStyles } from "../../constants/styles";
+import Animated, {
+  FadeIn,
+  FadeOutDown,
+  FadeOutLeft,
+  SlideInDown,
+  SlideInRight,
+  SlideInUp,
+  SlideOutDown,
+  SlideOutLeft,
+  ZoomIn,
+  ZoomInDown,
+  ZoomInUp,
+  ZoomOut,
+  ZoomOutDown,
+} from "react-native-reanimated";
+
 const loadingColor = GlobalStyles.colors.primaryGrayed;
 const SplashScreenOverlay = (containerStyle) => {
-  const image = { uri: "/assets/splash.png" };
   return (
     <>
-      <StatusBar hidden />
-      <View style={{ flex: 1 }}>
+      {/* <StatusBar hidden /> */}
+      <Animated.View
+        // entering={ZoomIn.duration(400)}
+        // exiting={SlideOutLeft.duration(800).delay(3500)}
+        // exiting={SlideOutDown.duration(1600).delay(3500)}
+        exiting={FadeOutDown.duration(800).delay(3500)}
+        style={{
+          flex: 1,
+          backgroundColor: GlobalStyles.colors.backgroundColor,
+        }}
+      >
         <ImageBackground
-          source={require("../../assets/splash.png")}
+          source={require("../../assets/LaunchScreenwhite2.png")}
           resizeMode={"cover"}
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
-          <View
+          <Animated.View
+            entering={ZoomInDown.duration(1200).delay(200)}
+            exiting={ZoomOutDown.duration(1200).delay(3000)}
             style={{
               paddingTop: "60%",
               flex: 1,
@@ -30,9 +56,9 @@ const SplashScreenOverlay = (containerStyle) => {
             }}
           >
             <ActivityIndicator size={"large"} color={loadingColor} />
-          </View>
+          </Animated.View>
         </ImageBackground>
-      </View>
+      </Animated.View>
     </>
   );
 };

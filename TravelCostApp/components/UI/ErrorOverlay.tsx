76084b0911af9b6097e9e58d0b21486839a15b20
Hannes Duve <hannes.duve@outlook.com>
2023-04-12 07:51:42 +0530

    cc

--- TravelCostApp/components/UI/ErrorOverlay.tsx
+++ TravelCostApp/components/UI/ErrorOverlay.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { GlobalStyles } from "../../constants/styles";
 import Button from "./Button";

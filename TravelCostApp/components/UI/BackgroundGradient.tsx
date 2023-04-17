345fc1727c8d8ecabc3d2d7905d5b97cd22e49d5
Hannes Duve <hannes.duve@outlook.com>
2023-04-17 10:58:13 +0530

    cc

--- TravelCostApp/components/UI/BackgroundGradient.tsx
+++ TravelCostApp/components/UI/BackgroundGradient.tsx
@@ -1,15 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import PropTypes from "prop-types";
+import { GlobalStyles } from "../../constants/styles";
 
 const BackgroundGradient = ({ colors, children, style }) => {
+  const defaultColors = GlobalStyles.gradientColors;
+  const useColors = colors ? colors : defaultColors;
   return (
-    <LinearGradient start={{ x: 2.3, y: 0.0 }} style={style} colors={colors}>
+    <LinearGradient start={{ x: 2.3, y: 0.0 }} style={style} colors={useColors}>
       {children}
     </LinearGradient>
   );
 };
 
 export default BackgroundGradient;
+
+BackgroundGradient.propTypes = {
+  colors: PropTypes.array,
+  children: PropTypes.node.isRequired,
+  style: PropTypes.object,
+};
 
 const styles = StyleSheet.create({});

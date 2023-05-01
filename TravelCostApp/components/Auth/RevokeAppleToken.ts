38e0e635c515aa2b769f640fa53b47c2f698e91d
Hannes Duve <hannes.duve@outlook.com>
2023-05-01 08:45:44 +0530

    prepared revoking apple token

---
+++ TravelCostApp/components/Auth/RevokeAppleToken.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+import qs from "qs";
+
+export async function revokeAppleToken(refreshToken) {
+  //   const client_secret = makeJWT(); // Assuming you have the makeJWT function in your project
+
+  const data = {
+    token: refreshToken,
+    client_id: "YOUR CLIENT ID",
+    // client_secret: client_secret,
+    token_type_hint: "refresh_token",
+  };
+
+  try {
+    const response = await axios.post(
+      "https://appleid.apple.com/auth/revoke",
+      qs.stringify(data),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+}

039f67468f595dfea3e08bffa1c586acdb22a52d
Hannes Duve <hannes.duve@outlook.com>
2023-05-06 09:02:05 +0530

    updated list of other keywords

--- TravelCostApp/components/ExpensesOutput/ExpenseItem.tsx
+++ TravelCostApp/components/ExpensesOutput/ExpenseItem.tsx
@@ -41,7 +41,7 @@ function ExpenseItem(props): JSX.Element {
     id,
     description,
     amount,
-    date,
+
     category,
     country,
     whoPaid,
@@ -51,6 +51,7 @@ function ExpenseItem(props): JSX.Element {
     iconName,
     showSumForTravellerName,
   } = props;
+  let { date } = props;
   const navigation = useNavigation();
   const tripCtx = useContext(TripContext);
   const userCtx = useContext(UserContext);
@@ -158,7 +159,7 @@ function ExpenseItem(props): JSX.Element {
     );
 
   if (!id) return <></>;
-
+  if (typeof date === "string") date = new Date(date);
   let dateString = date ? date : "no date";
   // if date is today, show "Today" instead of date
   // if periodName is "today" dont show "today"

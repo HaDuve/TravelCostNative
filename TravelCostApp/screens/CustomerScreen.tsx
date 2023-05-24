cb9df901eddbfc1d84eff1292470b5aa0d910be3
Hannes Duve <hannes.duve@outlook.com>
2023-05-24 15:17:09 +0530

    bump

---
+++ TravelCostApp/screens/CustomerScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CustomerScreen = () => {
+  return (
+    <View>
+      <Text>CustomerScreen</Text>
+    </View>
+  );
+};
+
+export default CustomerScreen;
+
+const styles = StyleSheet.create({});

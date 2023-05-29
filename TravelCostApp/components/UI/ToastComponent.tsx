8e125b0c8b4622f6015d439acaaab2ddb9d31148
Hannes Duve <hannes.duve@outlook.com>
2023-05-29 09:37:56 +0530

    added loading toast component

--- TravelCostApp/components/UI/ToastComponent.tsx
+++ TravelCostApp/components/UI/ToastComponent.tsx
@@ -2,6 +2,7 @@ import Toast, { BaseToast, ErrorToast } from "react-native-toast-message";
 import React from "react";
 import { View, Text } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
+import LoadingBarOverlay from "./LoadingBarOverlay";
 
 const toastConfig = {
   /*
@@ -57,12 +58,60 @@ const toastConfig = {
       I can consume any custom `props` I want.
       They will be passed when calling the `show` method (see below)
     */
-  // tomatoToast: ({ text1, props }) => (
-  //   <View style={{ height: 60, width: "100%", backgroundColor: "tomato" }}>
-  //     <Text>{text1}</Text>
-  //     <Text>{props.uuid}</Text>
-  //   </View>
-  // ),
+  loading: (props) => (
+    // props.progress - is a number from 0 to 1 or -1 (indeterminate)
+    <BaseToast
+      {...props}
+      style={[
+        {
+          borderLeftColor: GlobalStyles.colors.cat8,
+          backgroundColor: GlobalStyles.colors.backgroundColor,
+        },
+        GlobalStyles.wideStrongShadow,
+      ]}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+      text1Style={{
+        fontSize: 17,
+        fontWeight: "500",
+      }}
+      text2Style={{
+        fontSize: 15,
+        fontWeight: "400",
+      }}
+      // renderLeadingIcon={() => (
+      //   <View style={{ flex: 1, maxHeight: 55, maxWidth: 55 }}>
+      //     <LoadingBarOverlay></LoadingBarOverlay>
+      //   </View>
+      // )}
+      renderTrailingIcon={() => (
+        <View
+          style={{
+            flex: 1,
+            maxHeight: 55,
+            maxWidth: 55,
+            padding: 10,
+            marginRight: 10,
+          }}
+        >
+          <LoadingBarOverlay></LoadingBarOverlay>
+        </View>
+      )}
+      onPress={() => Toast.hide()}
+    />
+    // <View
+    //   style={[
+    //     {
+    //       borderLeftColor: GlobalStyles.colors.primary500,
+    //       borderLeftWidth: 5,
+    //       backgroundColor: GlobalStyles.colors.backgroundColor,
+    //     },
+    //     GlobalStyles.wideStrongShadow,
+    //   ]}
+    // >
+    //   <Text>{text1}</Text>
+    //   <Text>{text2}</Text>
+    // </View>
+  ),
   // call tomatoToast like this:
   // Toast.show({
   //   type: 'tomatoToast',

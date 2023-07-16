607705f650a95556cab34f200b431e89c32d9351
Hannes Duve <hannes.duve@outlook.com>
2023-07-16 16:00:13 +0530

    fix .env production problems

--- TravelCostApp/babel.config.js
+++ TravelCostApp/babel.config.js
@@ -1,17 +1,7 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(false);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      "react-native-reanimated/plugin",
-      [
-        "module:react-native-dotenv",
-        {
-          envName: "APP_ENV",
-          moduleName: "@env",
-          path: ".env",
-        },
-      ],
-    ],
+    plugins: ["react-native-reanimated/plugin", "module:react-native-dotenv"],
   };
 };

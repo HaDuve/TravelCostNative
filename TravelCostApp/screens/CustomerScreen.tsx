182cb4182d30161fd2cce7acdee3e92863a0d1a1
Hannes Duve <hannes.duve@outlook.com>
2023-09-19 12:01:36 +0530

    improved customer Screen

--- TravelCostApp/screens/CustomerScreen.tsx
+++ TravelCostApp/screens/CustomerScreen.tsx
@@ -1,8 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Platform } from "react-native";
 import React, { useEffect, useState } from "react";
 import Purchases, { CustomerInfo } from "react-native-purchases";
 import { ScrollView } from "react-native-gesture-handler";
 import { DateTime } from "luxon";
+import BackButton from "../components/UI/BackButton";
+import { Card } from "react-native-paper";
 
 const CustomerScreen = () => {
   const [customerInfo, setCustomerInfo] = useState<CustomerInfo>(null);
@@ -47,56 +49,59 @@ const CustomerScreen = () => {
   const billingIssueDetectedAt = entitlementInfo?.billingIssueDetectedAt;
 
   return (
-    <ScrollView style={styles.container}>
-      <View style={styles.section}>
-        <Text style={styles.label}>User ID:</Text>
-        <Text style={styles.info}>{originalAppUserID}</Text>
-      </View>
-      <View style={styles.section}>
-        <Text style={styles.label}>Active Subscriptions:</Text>
-        <Text style={styles.info}>{activeSubscriptions}</Text>
-      </View>
+    <Card style={styles.container}>
+      <ScrollView>
+        {/* <View style={styles.section}>
+          <Text style={styles.label}>User ID:</Text>
+          <Text style={styles.info}>{originalAppUserID}</Text>
+        </View> */}
+        <View style={styles.section}>
+          <Text style={styles.label}>Active Subscription:</Text>
+          <Text style={styles.info}>{activeSubscriptions}</Text>
+        </View>
+        {/*
+        <View style={styles.section}>
+          <Text style={styles.label}>Manage Subscription:</Text>
+          <Text style={styles.info}>{managementURL}</Text>
+        </View> */}
 
-      <View style={styles.section}>
-        <Text style={styles.label}>Manage Subscription:</Text>
-        <Text style={styles.info}>{managementURL}</Text>
-      </View>
+        <View style={styles.section}>
+          <Text style={styles.label}>Expiration Date:</Text>
+          <Text style={styles.info}>{expirationDate}</Text>
+        </View>
 
-      <View style={styles.section}>
-        <Text style={styles.label}>Expiration Date:</Text>
-        <Text style={styles.info}>{expirationDate}</Text>
-      </View>
+        <View style={styles.section}>
+          <Text style={styles.label}>Latest Purchase Date:</Text>
+          <Text style={styles.info}>{latestPurchaseDate}</Text>
+        </View>
 
-      <View style={styles.section}>
-        <Text style={styles.label}>Latest Purchase Date:</Text>
-        <Text style={styles.info}>{latestPurchaseDate}</Text>
-      </View>
+        {/* <View style={styles.section}>
+          <Text style={styles.label}>Original Purchase Date:</Text>
+          <Text style={styles.info}>{originalPurchaseDate}</Text>
+        </View> */}
 
-      <View style={styles.section}>
-        <Text style={styles.label}>Original Purchase Date:</Text>
-        <Text style={styles.info}>{originalPurchaseDate}</Text>
-      </View>
+        <View style={styles.section}>
+          <Text style={styles.label}>Period Type:</Text>
+          <Text style={styles.info}>{periodType}</Text>
+        </View>
 
-      <View style={styles.section}>
-        <Text style={styles.label}>Period Type:</Text>
-        <Text style={styles.info}>{periodType}</Text>
-      </View>
+        <View style={styles.section}>
+          <Text style={styles.label}>Will Renew:</Text>
+          <Text style={styles.info}>{willRenew ? "Yes" : "No"}</Text>
+        </View>
 
-      <View style={styles.section}>
-        <Text style={styles.label}>Will Renew:</Text>
-        <Text style={styles.info}>{willRenew}</Text>
-      </View>
+        <View style={styles.section}>
+          <Text style={styles.label}>Store:</Text>
+          <Text style={styles.info}>{store}</Text>
+        </View>
 
-      <View style={styles.section}>
-        <Text style={styles.label}>Store:</Text>
-        <Text style={styles.info}>{store}</Text>
-      </View>
-
-      <View style={styles.section}>
-        <Text style={styles.label}>Request Date:</Text>
-        <Text style={styles.info}>{reqDate}</Text>
-      </View>
-    </ScrollView>
+        <View style={styles.section}>
+          <Text style={styles.label}>Request Date:</Text>
+          <Text style={styles.info}>{reqDate}</Text>
+        </View>
+        <BackButton></BackButton>
+      </ScrollView>
+    </Card>
   );
 };
 
@@ -105,7 +110,10 @@ export default CustomerScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 24,
     padding: 16,
+    paddingTop: Platform.OS === "ios" ? 16 : 24,
+    marginTop: Platform.OS === "ios" ? 24 : 40,
   },
   section: {
     marginBottom: 16,

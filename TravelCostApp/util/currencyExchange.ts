e5305696820b2d5d481ae5f83e69164f20388002
Hannes Duve <hannes.duve@outlook.com>
2023-11-28 18:07:42 +0545

    improved currency api for other base countries

--- TravelCostApp/util/currencyExchange.ts
+++ TravelCostApp/util/currencyExchange.ts
@@ -3,14 +3,71 @@ import { DateTime } from "luxon";
 import { Alert } from "react-native";
 import { CACHE_NUM_HOURS, DEBUG_FORCE_OFFLINE } from "../confAppConstants";
 import {
+  asyncStoreGetItem,
   asyncStoreGetObject,
   asyncStoreSetItem,
   asyncStoreSetObject,
 } from "../store/async-storage";
 import { getMMKVString } from "../store/mmkv";
 import { secureStoreGetItem } from "../store/secure-storage";
+import safeLogError from "./error";
 
 export async function getRate(base: string, target: string) {
+  const response = await getRateAPI1(base, target);
+  console.log("getRate ~ initialresponse:", response);
+  if (typeof response === "number" && response === -1) {
+    try {
+      return await getRateAPI2(base, target);
+    } catch (error) {
+      safeLogError(error);
+      return -1;
+    }
+  }
+}
+
+export async function getRateAPI2(base: string, target: string) {
+  const lastUpdate = await asyncStoreGetItem("currencyExchangeAPI2_update");
+  if (lastUpdate) {
+    const lastUpdateDateTime = DateTime.fromISO(lastUpdate);
+    const now = DateTime.now();
+    const diff = now.diff(lastUpdateDateTime, "hours").hours;
+    console.log("getRateAPI2 ~ diff:", diff);
+    // this api is more restrictive, so we wait 4 hours
+    if (diff < CACHE_NUM_HOURS * 4) {
+      // get from asyncstore
+      const rate = await asyncStoreGetItem(
+        "currencyExchangeAPI2_" + base + target
+      );
+      if (rate) {
+        return rate;
+      }
+    }
+  }
+  const apiKey = await secureStoreGetItem("FREEEXCHANGE");
+  const requestURL =
+    "https://api.currencyapi.com/v3/latest?apikey=" +
+    apiKey +
+    "&currencies=" +
+    target +
+    "&base_currency=" +
+    base;
+  console.log("getRateAPI2 ~ requestURL:", requestURL);
+  try {
+    const response = await axios.get(requestURL);
+    const rate = response?.data?.data?.[target]?.value;
+    if (!rate) throw new Error("No rate found");
+    console.log("getRateAPI2 ~ rate:", rate);
+    const timeStamp = DateTime.now().toISO();
+    await asyncStoreSetItem("currencyExchangeAPI2_update", timeStamp);
+    await asyncStoreSetItem("currencyExchangeAPI2_" + base + target, rate);
+    return rate;
+  } catch (error) {
+    safeLogError(error);
+    return -1;
+  }
+}
+
+export async function getRateAPI1(base: string, target: string) {
   console.log("getRate ~ target:", target);
   console.log("getRate ~ base:", base);
   if (base === target) {

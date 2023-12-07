34a9c5ebdc1a6028fdbdc339fb90b781b9102aae
Hannes Duve <hannes.duve@outlook.com>
2023-12-07 08:29:36 +0700

    sorted for frequency used

--- TravelCostApp/confAppConstants.ts
+++ TravelCostApp/confAppConstants.ts
@@ -1,13 +1,17 @@
-// asyncStorage, set to true if you want all storage to be reset and user logged out
-export const DEBUG_RESET_STORAGE = false;
+// DEV const for testing and getting developer information
+export const DEVELOPER_MODE = false;
+
 // OfflineMode, set to true if you want the simulator to be offline
 export const DEBUG_FORCE_OFFLINE = false;
+// asyncStorage, set to true if you want all storage to be reset and user logged out
+export const DEBUG_RESET_STORAGE = false;
+// tweak minimum required internet speed in Mbit/s
+export const MINIMUM_REQUIRED_SPEED = 0.1;
+
 // no data, set to true if you want to load no data
 export const DEBUG_NO_DATA = false;
 // polling interval data
 export const DEBUG_POLLING_INTERVAL = 5000;
-// DEV const for testing and getting developer information
-export const DEVELOPER_MODE = false;
 // Premium consts
 export const FORCE_PREMIUM = false;
 // Rating/Review consts
@@ -24,8 +28,6 @@ export const MAX_JS_NUMBER = 3435973836;
 export const SPLASH_SCREEN_DELAY = 1500;
 // tweak loading recent expenses overlay timeout
 export const EXPENSES_LOAD_TIMEOUT = 2000;
-// tweak minimum required internet speed in Mbit/s
-export const MINIMUM_REQUIRED_SPEED = 0.1;
 // cache: only call the exchange getRate request again after X numbers of hours
 export const CACHE_NUM_HOURS = 1;
 // maximum number of expenses before we start to show cheap text-based expenseItems

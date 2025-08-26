4b9490d3668fcdf8a65f3f4fb7037e4b19d86114
Hannes Duve <hannes.duve@outlook.com>
2025-08-26 15:55:27 +0800

    UPDATE: update deps

--- TravelCostApp/metro.config.js
+++ TravelCostApp/metro.config.js
@@ -4,4 +4,7 @@ const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push("mjs");
 defaultConfig.resolver.assetExts.push("cjs");
 
+// Disable package exports enforcement if needed for compatibility with older libraries
+defaultConfig.resolver.unstable_enablePackageExports = false;
+
 module.exports = defaultConfig;
